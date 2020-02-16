5e951235433e53a4b7811564471c0ff6edc05836
vladimir <lovkiy2012@gmail.com>
2020-02-16 19:49:06 +0300

    11 styles

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}> Привет nhh </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#000",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  text: {
+    color: "#fff",
+    fontSize: 26
+  }
+});
