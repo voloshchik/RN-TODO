cc143b6455333a48e91afd2de5dbe6907b593275
vladimir <lovkiy2012@gmail.com>
2020-02-17 18:51:31 +0300

    21

--- App.js
+++ App.js
@@ -1,31 +1,52 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Alert, ScrollView } from "react-native";
 import Navbar from "./src/Navbar";
 import AddTodo from "./src/AddTodo";
-import Todo from './src/Todo';
+import Todo from "./src/Todo";
 
 export default function App() {
-  const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState([
+    {
+      id: "1",
+      title: " поченить поло"
+    },
+    {
+      id: "1",
+      title: " поченить поло"
+    },
+    {
+      id: "2",
+      title: " поченить поло"
+    },
+    {
+      id: "3",
+      title: " поченить поло"
+    },
+    {
+      id: "4",
+      title: " поченить поло"
+    }
+  ]);
   const addTodo = title => {
     const newTodo = {
-      id: Date.now.toString(),
+      id: Date.now().toString(),
       title
     };
-    setTodos(prev=>[...prev,newTodo])
-  
+    setTodos(prev => [...prev, newTodo]);
   };
   return (
-    <View>
+    <ScrollView>
       <Navbar title="Todo App!" />
       <View style={styles.container}>
-        <AddTodo  onSubmit={addTodo}/>
+        <AddTodo onSubmit={addTodo} />
         <View>
-          {todos.map((todo)=>{
-            return <Todo todo={todo} key={todo.id}/>
+          {todos.map(todo => {
+            // Alert.alert(todo.id);
+            return <Todo todo={todo} key={todo.id} />;
           })}
         </View>
       </View>
-    </View>
+    </ScrollView>
   );
 }
 
--- src/AddTodo.js
+++ src/AddTodo.js
@@ -1,39 +1,41 @@
-import React,{useState} from "react";
-import { View, Text, StyleSheet, TextInput, Button } from "react-native";
+import React, { useState } from "react";
+import { View, Text, StyleSheet, TextInput, Button, Alert } from "react-native";
 
-const AddTodo = ({onSubmit}) => {
-    const [value,setValue] = useState('')
-const pressHandler=()=>{
-    if(value.trim()){
-        onSubmit(value)
-        setValue('')
-    }else{
-
+const AddTodo = ({ onSubmit }) => {
+  const [value, setValue] = useState("");
+  const pressHandler = () => {
+    if (value.trim()) {
+      onSubmit(value);
+      setValue("");
+    } else {
+      Alert.alert("дело не можент быть пустым");
     }
-    
-}
+  };
   return (
     <View style={styles.block}>
-       
-      <TextInput style={styles.input}  placeholder='Бяйго Женя' onChangeText={(text)=>setValue(text)} value={value}/>
-      <Button title="Добавит" onPress={pressHandler} />
-      
+      <TextInput
+        style={styles.input}
+        placeholder="Бяйго Женя"
+        onChangeText={text => setValue(text)}
+        value={value}
+      />
+      <Button title="Добавить" onPress={pressHandler} />
     </View>
   );
 };
 const styles = StyleSheet.create({
   block: {
     flexDirection: "row",
-    justifyContent:'space-between',
-    alignItems:'center',
-    marginBottom:15
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 15
   },
   input: {
     width: "70%",
-    padding:10,
+    padding: 10,
     borderStyle: "solid",
     borderBottomColor: "#3949ab",
-    borderBottomWidth:2
+    borderBottomWidth: 2
   }
 });
 export default AddTodo;
