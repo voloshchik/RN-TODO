d6e754d6e24cbd4a01b1d4b9c10764d34e6d9c60
vladimir <lovkiy2012@gmail.com>
2020-02-18 21:20:38 +0300

    28 THEME

--- src/screens/TodoScreen.js
+++ src/screens/TodoScreen.js
@@ -1,18 +1,18 @@
 import React from "react";
 import { View, Text, StyleSheet, Button } from "react-native";
-
+import {THEME} from '../theme'
 const TodoSreen = ({ goBack, todo }) => {
   return (
     <View>
       <Text>{todo.title}</Text>
       <View style={styles.buttons}>
         <View style={styles.button}>
-          <Button title="Назад" onPress={goBack} color="#757575" />
+          <Button title="Назад" onPress={goBack} color={THEME.GREY_COLOR} />
         </View>
         <View style={styles.button}>
           <Button
             title="Удалить"
-            color="#e53935"
+            color={THEME.DANGER_COLOR}
             onPress={goBack}
             onPress={() => console.log("delete")}
           />
