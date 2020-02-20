86e98c3918f6912d675ef84a1b61576e617f1d6e
vladimir <lovkiy2012@gmail.com>
2020-02-20 19:26:24 +0300

    33 image

--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -1,20 +1,25 @@
 import React from "react";
-import { View, StyleSheet, FlatList } from "react-native";
+import { View, StyleSheet, FlatList, Text } from "react-native";
 import AddTodo from "../components/AddTodo";
 import Todo from "../components/Todo";
 
 const MainScreen = ({ todos, addTodo, removeTodo, openTodo }) => {
+  if (todos.length === 0) {
+    return <Text>ddddd</Text>;
+  }
+  let content = (
+    <FlatList
+      keyExtractor={item => item.id.toString()}
+      data={todos}
+      renderItem={({ item }) => (
+        <Todo todo={item} onRemove={removeTodo} onOpen={openTodo} />
+      )}
+    />
+  );
   return (
     <View>
       <AddTodo onSubmit={addTodo} />
-
-      <FlatList
-        keyExtractor={item => item.id.toString()}
-        data={todos}
-        renderItem={({ item }) => (
-          <Todo todo={item} onRemove={removeTodo} onOpen={openTodo} />
-        )}
-      />
+      {content}
     </View>
   );
 };
