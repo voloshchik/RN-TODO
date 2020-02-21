050175e9bcfbc51b94e7f0453a103b8d60567a68
vladimir <lovkiy2012@gmail.com>
2020-02-21 20:43:08 +0300

    37 add icon

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { View, StyleSheet, TextInput, Button, Alert } from "react-native";
 import { THEME } from "../theme";
-
+import { AntDesign } from "@expo/vector-icons";
 const AddTodo = ({ onSubmit }) => {
   const [value, setValue] = useState("");
 
@@ -24,7 +24,10 @@ const AddTodo = ({ onSubmit }) => {
         autoCorrect={false}
         autoCapitalize="none"
       />
-      <Button title="Добавить" onPress={pressHandler} />
+      <AntDesign.Button onPress={pressHandler} name="pluscircleo">
+        Добавить
+      </AntDesign.Button>
+      {/* <Button title="Добавить" onPress={pressHandler} /> */}
     </View>
   );
 };
@@ -37,7 +40,7 @@ const styles = StyleSheet.create({
     marginBottom: 15
   },
   input: {
-    width: "70%",
+    width: "60%",
     padding: 10,
     borderStyle: "solid",
     borderBottomWidth: 2,
