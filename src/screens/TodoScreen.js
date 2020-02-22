fbe534dbdb9c2ac16ebbd9da8c88cb6eddcf13b0
vladimir <lovkiy2012@gmail.com>
2020-02-22 19:20:43 +0300

    41 Dimensions

--- src/screens/TodoScreen.js
+++ src/screens/TodoScreen.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
+import { View, Text, StyleSheet,Dimensions } from "react-native";
 import { FontAwesome, AntDesign } from "@expo/vector-icons";
 import { THEME } from "../theme";
 import AppCard from "../components/ui/AppCard";
@@ -53,7 +53,7 @@ const styles = StyleSheet.create({
     justifyContent: "space-between"
   },
   button: {
-    width: "40%"
+    width:Dimensions.get('window').width/3
   },
   title: {
     fontSize: 20
