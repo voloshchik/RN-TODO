909ec02f016cc9f5ddba0b07d32114484189e5ef
vladimir <lovkiy2012@gmail.com>
2020-02-22 18:49:40 +0300

    40 Platform

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,19 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Platform } from "react-native";
 import { THEME } from "../theme";
-import AppTextBold from './ui/AppTextBold';
+import AppTextBold from "./ui/AppTextBold";
 
 const Navbar = ({ title }) => {
   return (
-    <View style={styles.navbar}>
+    <View
+      style={{
+        ...styles.navbar,
+        ...Platform.select({
+          ios: styles.navbarIos,
+          android: styles.navbarAndroid
+        })
+      }}
+    >
       <AppTextBold style={styles.text}>{title}</AppTextBold>
     </View>
   );
@@ -16,11 +24,18 @@ const styles = StyleSheet.create({
     height: 70,
     alignItems: "center",
     justifyContent: "flex-end",
-    backgroundColor: THEME.MAIN_COLOR,
+
     paddingBottom: 10
   },
+  navbarAndroid: {
+    backgroundColor: THEME.MAIN_COLOR
+  },
+  navbarIos: {
+    borderBottomColor: THEME.MAIN_COLOR,
+    borderBottomWidth: 1
+  },
   text: {
-    color: "white",
+    color:Platform.OS==='android'?'#fff':THEME.MAIN_COLOR,
     fontSize: 20
   }
 });
--- src/components/ui/AppButton.js
+++ src/components/ui/AppButton.js
@@ -1,15 +1,23 @@
 import React, { Children } from "react";
-import { View, StyleSheet, TouchableOpacity } from "react-native";
+import {
+  View,
+  StyleSheet,
+  TouchableOpacity,
+  Platform,
+  TouchableNativeFeedback
+} from "react-native";
 import AppTextBold from "./AppTextBold";
 import { THEME } from "../../theme";
 
 const AppButton = ({ children, onPress, color = THEME.MAIN_COLOR }) => {
+  const Wrapper =
+    Platform.OS === "android" ? TouchableNativeFeedback : TouchableOpacity;
   return (
-    <TouchableOpacity onPress={onPress} activeOpacity={0.7}>
+    <Wrapper onPress={onPress} activeOpacity={0.7}>
       <View style={{ ...styles.button, backgroundColor: color }}>
         <AppTextBold style={styles.text}>{children}</AppTextBold>
       </View>
-    </TouchableOpacity>
+    </Wrapper>
   );
 };
 const styles = StyleSheet.create({
