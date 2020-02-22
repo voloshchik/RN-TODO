02b265a72ea6f71c2c44330dfb855f886d65c552
vladimir <lovkiy2012@gmail.com>
2020-02-22 22:24:20 +0300

    42

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ import { AppLoading } from 'expo'
 import Navbar from "./src/components/Navbar";
 import MainScreen from "./src/screens/MainScreen";
 import TodoSreen from "./src/screens/TodoScreen";
+import { THEME } from './src/theme';
 async function loadApplication() {
   await Font.loadAsync({
     "roboto-regular": require("./assets/fonts/Roboto-Regular.ttf"),
@@ -105,7 +106,7 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    paddingHorizontal: 30,
+    paddingHorizontal:THEME.PADDING_HORIZONTAL,
     paddingVertical: 20
   }
 });
--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -1,17 +1,42 @@
-import React from "react";
-import { View, StyleSheet, FlatList, Text, Image } from "react-native";
+import React, { useState, useEffect } from "react";
+import {
+  View,
+  StyleSheet,
+  FlatList,
+  Text,
+  Image,
+  Dimensions
+} from "react-native";
 import AddTodo from "../components/AddTodo";
 import Todo from "../components/Todo";
+import { THEME } from "../theme";
 
 const MainScreen = ({ todos, addTodo, removeTodo, openTodo }) => {
+  const [deviceWith, setDeviceWith] = useState(
+    Dimensions.get("window").width - THEME.PADDING_HORIZONTAL * 2
+  );
+
+  useEffect(() => {
+    const update = () => {
+      const width =
+        Dimensions.get("window").width - THEME.PADDING_HORIZONTAL * 2;
+      setDeviceWith(width);
+    };
+    Dimensions.addEventListener("change", update);
+    return () => {
+      Dimensions.removeEventListener("change", update);
+    };
+  });
   let content = (
-    <FlatList
-      keyExtractor={item => item.id.toString()}
-      data={todos}
-      renderItem={({ item }) => (
-        <Todo todo={item} onRemove={removeTodo} onOpen={openTodo} />
-      )}
-    />
+    <View style={{ width: deviceWith }}>
+      <FlatList
+        keyExtractor={item => item.id.toString()}
+        data={todos}
+        renderItem={({ item }) => (
+          <Todo todo={item} onRemove={removeTodo} onOpen={openTodo} />
+        )}
+      />
+    </View>
   );
   if (todos.length === 0) {
     content = (
